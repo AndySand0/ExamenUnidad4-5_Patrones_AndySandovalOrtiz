27faaa3c9005215a5ad06852a58cd5209f132fb1
AndySand0 <l21210547@tectijuana.edu.mx>
2025-12-11 19:19:15 -0800

    Delete src/CategoriaDecorator.cs

--- src/CategoriaDecorator.cs
+++
@@ -1,7 +0,0 @@
-public class CategoriaDecorator : ProductoDecorator
-{
-    private readonly string _categoria;
-    public CategoriaDecorator(IProducto producto, string categoria) : base(producto) => _categoria = categoria;
-
-    public override string ObtenerDescripcion() => base.ObtenerDescripcion() + $" | Categoría: {_categoria}";
-}
