397dd4ef9f27771975572df8f7fc62b8bfda4b01
AndySand0 <l21210547@tectijuana.edu.mx>
2025-12-11 19:17:30 -0800

    Add ProductoRepository for managing products

---
+++ src/Models/Repository/ProductoRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+public class ProductoRepository
+{
+    private readonly Dictionary<int, IProducto> _productos = new();
+
+    public void Guardar(IProducto p) => _productos[p.Id] = p;
+    public IProducto Obtener(int id) => _productos.ContainsKey(id) ? _productos[id] : null;
+    public IEnumerable<IProducto> ObtenerTodos() => _productos.Values;
+    public void Eliminar(int id) => _productos.Remove(id);
+}
