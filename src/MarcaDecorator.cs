017456af1e428db91a6ee22f3aa324018c9de6ce
AndySand0 <l21210547@tectijuana.edu.mx>
2025-12-11 19:21:28 -0800

    Delete src/MarcaDecorator.cs

--- src/MarcaDecorator.cs
+++
@@ -1,7 +0,0 @@
-public class MarcaDecorator : ProductoDecorator
-{
-    private readonly string _marca;
-    public MarcaDecorator(IProducto producto, string marca) : base(producto) => _marca = marca;
-
-    public override string ObtenerDescripcion() => base.ObtenerDescripcion() + $" | Marca: {_marca}";
-}
